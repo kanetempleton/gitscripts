6bbb135ae4f4023c0da030024f63d47211ae3ec4
kanetempleton <ktempleton96@yahoo.com>
2022-07-28 12:43:35 -0500

    first commit

--- git-newrepo.sh
+++ git-newrepo.sh
@@ -9,6 +9,6 @@ git init
 git add .
 git commit -a -m 'first commit'
 git branch -M main
-git remote add origin "git@githubcom:kanetempleton/$1.git"
+git remote add origin "git@github.com:kanetempleton/$1.git"
 git push origin main
 
